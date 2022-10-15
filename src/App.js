9bdba8ab17d3764a30ec42a75fa48a04b5dcd7dd
SparkyFnay <z1362110538@gmail.com>
2022-10-15 00:58:01 -0400

    changed url

--- src/App.js
+++ src/App.js
@@ -1,42 +1,55 @@
 import "./index.css";
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import Servers from './components/Servers';
-import Server from './components/Server';
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Servers from "./components/Servers";
+import Server from "./components/Server";
 
 export default function App() {
-  const [serverData, setServerData] = useState([])
-  const [showAllServers, setShowAllServers] = useState(true)
-  const [server, setServer] = useState('')
+  const [serverData, setServerData] = useState([]);
+  const [showAllServers, setShowAllServers] = useState(true);
+  const [server, setServer] = useState("");
 
   const pickServer = (id) => {
-    var res = serverData.find(server=>server.server_id==id);
-    setServer (res)
-    setShowAllServers (false)
-  }
+    var res = serverData.find((server) => server.server_id == id);
+    setServer(res);
+    setShowAllServers(false);
+  };
   useEffect(() => {
-    fetchStuff()
-  }, [])
+    fetchStuff();
+  }, []);
 
   const fetchStuff = () => {
-    axios.get('https://my-discord-bot-data.herokuapp.com/login/servers/')
+    axios
+      .get("http://143.244.203.14:8000/login/servers")
       .then((res) => {
-        setServerData(res.data.data)
+        setServerData(res.data.data);
       })
       .then(function () {
         // always executed
       });
-  }
+  };
 
   return (
     <div className="App">
-      <div className = "container">
-        <div className = "header">
+      <div className="container">
+        <div className="header">
           <h1>My Discord Bot Data</h1>
-          {!showAllServers && <button className = "btn-inline" onClick = {() => {setShowAllServers(true)}}>Back</button>}
+          {!showAllServers && (
+            <button
+              className="btn-inline"
+              onClick={() => {
+                setShowAllServers(true);
+              }}
+            >
+              Back
+            </button>
+          )}
         </div>
-        {showAllServers ? <Servers servers = {serverData} onPickServer = {pickServer}/> : <Server server = {server}/>}
+        {showAllServers ? (
+          <Servers servers={serverData} onPickServer={pickServer} />
+        ) : (
+          <Server server={server} />
+        )}
       </div>
     </div>
   );
--- src/components/Server.js
+++ src/components/Server.js
@@ -1,29 +1,34 @@
 import "../index.css";
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import UserInServer from './UserInServer';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import UserInServer from "./UserInServer";
 
-const Server = ({server}) => {
-  const [serverData, setServerData] = useState([])
+const Server = ({ server }) => {
+  const [serverData, setServerData] = useState([]);
 
   useEffect(() => {
-    fetchStuff()
-  }, [])
-
+    fetchStuff();
+  }, []);
 
   const fetchStuff = async () => {
-    var res = await axios.get('https://my-discord-bot-data.herokuapp.com/login/servers/'+server.server_id+'/');
-     setServerData(res.data.data)
-  }
+    var res = await axios.get(
+      "http://143.244.203.14:8000/login/servers" + server.server_id + "/"
+    );
+    setServerData(res.data.data);
+  };
 
-  const name = server.Name
+  const name = server.Name;
   return (
     <div>
       <div>
-        {serverData.length == 0 ? <h1>Loading ... Please Wait</h1> : <UserInServer userArray = {serverData} serverName = {name}/>}
+        {serverData.length == 0 ? (
+          <h1>Loading ... Please Wait</h1>
+        ) : (
+          <UserInServer userArray={serverData} serverName={name} />
+        )}
       </div>
     </div>
   );
-}
+};
 
-export default Server
+export default Server;
